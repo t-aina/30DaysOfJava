47f88f4330200e645605e4664f4b7c6aac8440c5
Oluwatobi Aina <tobi@Tobi.local>
2020-04-12 17:16:26 -0500

    initial commit

---
+++ MySweetProgram/src/SweetProgram.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.math.BigDecimal;
+
+public class SweetProgram {
+
+		public static void main(String[] args) { 
+			
+			Scanner in = new Scanner(System.in);
+			//String s = in.nextLine();
+	
+		int i = 0; 
+		do {
+			System.out.println("i is " + i);
+			i++;
+		}while(false);
+		
+	}
+
+}
